a99d49a9059d56af74ffe9d00e6d54e0dbf0e7b1
Jussif79 <jussif.79@gmail.com>
2024-09-30 14:57:17 +0200

    Update MainActivity.kt

--- app/src/main/java/ch/jussif/tictactoe/MainActivity.kt
+++ app/src/main/java/ch/jussif/tictactoe/MainActivity.kt
@@ -5,12 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import ch.jussif.tictactoe.ui.theme.TicTacToeTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,10 +45,51 @@ fun TicTacToe(viewModel: TTTViewModel) {
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Spacer(modifier = Modifier.height(20.dp))
+        ServerBar(viewModel)
+        Spacer(modifier = Modifier.height(20.dp))
+        RegisterBar(viewModel)
+        Spacer(modifier = Modifier.height(20.dp))
+        LoginBar(viewModel)
+        Spacer(modifier = Modifier.height(20.dp))
+        GameBar(viewModel)
+        Spacer(modifier = Modifier.height(20.dp))
+        GameArea(viewModel)
     }
 }
 
-fun ServerBar(viewModel: TTTViewModel){
+@Composable
+fun ServerBar(viewModel: TTTViewModel) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Server: " + Base_URL)
+        Button(onClick = { viewModel.ping() }) { Text(text = "Ping")}
+        Text(text = viewModel.pingResult.toString())
+    }
+}
+
+@Composable
+fun RegisterBar(viewModel: TTTViewModel) {
+    var userName by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+
+
+}
+
+@Composable
+fun LoginBar(viewModel: TTTViewModel) {
+
+}
+
+@Composable
+fun GameBar(viewModel: TTTViewModel) {
+
+}
+
+@Composable
+fun GameArea(viewModel: TTTViewModel) {
 
 }
